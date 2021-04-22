4ff07c867e7a3aab62b914e1c9e6481ae86cd1db
Olha Tarasova <tarasovao0505@gmail.com>
2021-04-22 21:52:29 +0300

    Добавлен слайд

---
+++ js/script.js
@@ -0,0 +1,11 @@
+new Swiper('.image-slider', {
+    
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true,
+    },
+
+    touchRatio: 5,
+    grabCursor: true,
+});
